ac1445bc945b31a3f22d983a8d38b19de6a710d1
Ilya Visnikin <rarebloor@gmail.com>
2023-05-15 01:27:00 +0300

    Added ability to extract configuration file location form cli

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"github.com/IlyaVishnikin/http-rest-api/internal/apiserver"
+	"github.com/IlyaVishnikin/http-rest-api/internal/logger"
+)
 
+const (
+	infoLogPath = "log/info.log"
+	warnLogPath = "log/warn.log"
+	errLogPath  = "log/err.log"
+)
+
+var (
+	configPath = flag.String("conf", "config/apiserver.json", "Server configuration file location")
+)
+
+func main() {
+	flag.Parse()
+	logger.Init(infoLogPath, warnLogPath, errLogPath)
+	defer logger.Close()
+	conf := apiserver.NewConfig(*configPath)
+	serv := apiserver.New(conf)
 }
